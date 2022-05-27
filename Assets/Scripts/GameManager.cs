b16527b4929b85edb35bc06a1e4bea2776204e41
Steve <rreka777@gmail.com>
2022-05-27 15:00:24 +0800

    - Refactored MazeControls.cs - Renamed GameEnv to GameManger - Renamed Input Actions "Hedgeball" to "PlayerInput" - Reorganized directories and files

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public sealed class GameEnv
+public sealed class GameManager
 {
-    public static GameEnv instance;
+    public static GameManager instance;
     private List<GameObject> objects = new(); //dummy list
     private List<GameObject> checkpoints = new(); //dummy list
     private List<GameObject> enemies = new(); //dummy list
 
     public List<GameObject> Objects { get => objects; }
 
-    public static GameEnv Singleton
+    public static GameManager Singleton
     {
         get 
         {
             if (instance == null)
             {
-                instance = new GameEnv();
+                instance = new GameManager();
                 instance.checkpoints.AddRange(GameObject.FindGameObjectsWithTag("checkpoints"));
                 instance.enemies.AddRange(GameObject.FindGameObjectsWithTag("enemies"));
             }
