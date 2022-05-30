d1fae6d53d6e758e7f98e287292f0d58a7e0a10d
Steve <rreka777@gmail.com>
2022-05-30 14:13:43 +0800

    Renamed GameManager to GameEnv

--- Assets/Scripts/GameEnv.cs
+++ Assets/Scripts/GameEnv.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public sealed class GameManager
+public sealed class GameEnv
 {
-    public static GameManager instance;
+    public static GameEnv instance;
     private List<GameObject> objects = new(); //dummy list
     private List<GameObject> checkpoints = new(); //dummy list
     private List<GameObject> enemies = new(); //dummy list
 
     public List<GameObject> Objects { get => objects; }
 
-    public static GameManager Singleton
+    public static GameEnv Singleton
     {
         get 
         {
             if (instance == null)
             {
-                instance = new GameManager();
+                instance = new GameEnv();
                 instance.checkpoints.AddRange(GameObject.FindGameObjectsWithTag("checkpoints"));
                 instance.enemies.AddRange(GameObject.FindGameObjectsWithTag("enemies"));
             }
